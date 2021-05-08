28590911fde3bb4598c737f9ffb05a250e7a9f90
chemicalfiber <q1828167322@163.com>
2021-05-08 11:36:14 +0800

    可以从「上传日志」页面删除以前上传的文件和数据库中对应的记录

--- src/main/java/cn/cf/webresourcemanagement/controller/UploadList.java
+++ src/main/java/cn/cf/webresourcemanagement/controller/UploadList.java
@@ -2,11 +2,13 @@ package cn.cf.webresourcemanagement.controller;
 
 import cn.cf.webresourcemanagement.eneity.FileInfo;
 import cn.cf.webresourcemanagement.repository.FileInfoRepository;
+import cn.cf.webresourcemanagement.util.GetPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.File;
 import java.util.List;
 
 @Controller
@@ -16,8 +18,19 @@ public class UploadList {
 
     @GetMapping("/list")
     public String toUploadList(Model model){
-        List<FileInfo> uploadInfoList = fileInfoRepository.findAll();
-        model.addAttribute("uploadList",uploadInfoList);
+        model.addAttribute("uploadList",fileInfoRepository.findAll());
         return "list";
+    }
+
+    @GetMapping("/deleteFile")
+    public String deleteFile(String filename,Model model){
+        String filePath = GetPath.getUploadPath();
+        File file = new File(filePath + File.separator + filename);
+        System.out.println("要删除的文件是：" + filename);
+        file.delete();
+        fileInfoRepository.delete(fileInfoRepository.findByFilename(filename));
+        model.addAttribute("deleteStatus","删除成功！");
+        model.addAttribute("uploadList",fileInfoRepository.findAll());
+        return "/list";
     }
 }
--- src/main/java/cn/cf/webresourcemanagement/repository/FileInfoRepository.java
+++ src/main/java/cn/cf/webresourcemanagement/repository/FileInfoRepository.java
@@ -4,4 +4,5 @@ import cn.cf.webresourcemanagement.eneity.FileInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileInfoRepository extends JpaRepository<FileInfo,Integer> {
+    public FileInfo findByFilename(String filename);
 }
